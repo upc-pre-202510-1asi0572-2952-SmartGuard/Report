f8f6aa3c470e0e512d0f1c59ab6760368ba1de29
Fernando <faquino@pecano.pe>
2025-05-14 12:17:24 -0500

    add: archivos netlify

--- Smart-Guard-Fronted/vite.config.ts
+++ Smart-Guard-Fronted/vite.config.ts
@@ -7,6 +7,7 @@ import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     tailwindcss(),
